746ea2e8d67efc653f080dccb8e88371156e4ab1
elite2012 <liu_wei75@hotmail.com>
2023-04-08 13:21:57 +0800

    使用 JSX 展开语法传递 props

---
+++ my-app/src/index-ui-passingpropstocomponent.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './UI/propscomponent/passingpropstocomponent/App';
+//UI-props-向组件传递 props
+ReactDOM.render(
+    <App/>,
+    document.getElementById('root')
+
+)
--- my-app/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './UI/jsx/js/jscurlybraces/obj/App';
-//UI-第一个组件-jsx-使用引号传递字符串
-ReactDOM.render(
-    <App/>,
-    document.getElementById('root')
-
-)
