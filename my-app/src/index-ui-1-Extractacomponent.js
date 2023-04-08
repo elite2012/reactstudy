d300eccabc419e96445e2117fa5367b8a3a0b5b7
elite2012 <liu_wei75@hotmail.com>
2023-04-08 16:18:53 +0800

    调整图像大小

---
+++ my-app/src/index-ui-1-Extractacomponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './UI/propscomponent/reacp/1-Extractacomponent/App'
+//这个 Gallery 组件包含两份个人资料，其中有一些非常相似的标签
+//。从中提取一个 Profile 组件以减少重复。你需要选择要传递哪些 props。
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById('root')
+
+)
