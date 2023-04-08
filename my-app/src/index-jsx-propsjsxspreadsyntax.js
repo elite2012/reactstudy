9139fb2f683ab446001c203754ae9a1ea5ef2a6c
elite2012 <liu_wei75@hotmail.com>
2023-04-08 14:20:55 +0800

    UI-props-将 JSX 作为子组件传递

---
+++ my-app/src/index-jsx-propsjsxspreadsyntax.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './UI/propscomponent/propsjsxspreadsyntax/App';
+//UI-props-向组件传递 props-未实现
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById('root')
+
+)
