bc1a1d9b6783cc29d65d518e03ed73944251206e
elite2012 <liu_wei75@hotmail.com>
2023-04-13 20:31:03 +0800

    阻止传播

---
+++ my-app/src/index-Adding Interactivity-1-Responding to events-4-Stopping propagation.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './Adding Interactivity/1-Responding to events/4-stopping propagation/App';
+//阻止传播
+ReactDOM.render(
+    <App/>,
+    document.getElementById('root')
+
+)
