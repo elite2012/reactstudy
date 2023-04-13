98f4758dd6aaf8f3cfe9127fe61d22c68f14740f
elite2012 <liu_wei75@hotmail.com>
2023-04-13 15:42:13 +0800

    纯函数：组件作为公式

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './UI/8-Keeping Components Pure/1-PurityComponents as formulas/App
+import App from './UI/8-Keeping Components Pure/1-PurityComponents as formulas/App';
 //纯函数：组件作为公式 
 ReactDOM.render(
     <App/>,
